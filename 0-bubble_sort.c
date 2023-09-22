4a0a50be369d601c7384d8c4668ba830e274f52e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-22 21:29:19 +0100

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - sorts an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    int pass, i, temp;
+    for (pass = size - 1 ; pass >= 0; pass--)
+    {
+        for (i = 0; i < pass; i++ )
+        {
+            if (array[i] > array[i + 1])
+            {
+                 temp = array[i];
+                 array[i] = array[i + 1];
+                 array[i + 1] = temp;
+            }
+           
+               
+        }
+        for (i = 0; i < size; i++)
+        {
+            if (i > 0)
+                printf(", ");
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+    }
+
+}
